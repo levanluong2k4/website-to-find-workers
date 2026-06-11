009b5317fd6374cac53a6bfc74bb1df8d76988ce
levanluong2k4 <levanluong18t@gmail.com>
2026-06-12 00:59:00 +0700

    fix: regenerate bootstrap cache WITHOUT dev packages

--- bootstrap/cache/packages.php
+++ bootstrap/cache/packages.php
@@ -6,31 +6,6 @@
       0 => 'CloudinaryLabs\\CloudinaryLaravel\\CloudinaryServiceProvider',
     ),
   ),
-  'fruitcake/laravel-debugbar' => 
-  array (
-    'aliases' => 
-    array (
-      'Debugbar' => 'Fruitcake\\LaravelDebugbar\\Facades\\Debugbar',
-    ),
-    'providers' => 
-    array (
-      0 => 'Fruitcake\\LaravelDebugbar\\ServiceProvider',
-    ),
-  ),
-  'laravel/pail' => 
-  array (
-    'providers' => 
-    array (
-      0 => 'Laravel\\Pail\\PailServiceProvider',
-    ),
-  ),
-  'laravel/sail' => 
-  array (
-    'providers' => 
-    array (
-      0 => 'Laravel\\Sail\\SailServiceProvider',
-    ),
-  ),
   'laravel/sanctum' => 
   array (
     'providers' => 
@@ -50,13 +25,6 @@
     'providers' => 
     array (
       0 => 'Carbon\\Laravel\\ServiceProvider',
-    ),
-  ),
-  'nunomaduro/collision' => 
-  array (
-    'providers' => 
-    array (
-      0 => 'NunoMaduro\\Collision\\Adapters\\Laravel\\CollisionServiceProvider',
     ),
   ),
   'nunomaduro/termwind' => 
