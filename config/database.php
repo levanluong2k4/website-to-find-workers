df6b59cad54a9030f7c6fd2cd3463f87cd7d1acb
levanluong2k4 <levanluong18t@gmail.com>
2026-06-13 18:31:29 +0700

    fix: deprecated PDO constant breaking PHP 8.4+ on Vercel

--- config/database.php
+++ config/database.php
@@ -76,9 +76,9 @@ return [
             'prefix' => '',
             'strict' => false,
             'engine' => null,
-            'options' => array_filter([
-                PDO::MYSQL_ATTR_SSL_CA => base_path('storage/cacert.pem'),
-            ]),
+            'options' => extension_loaded('pdo_mysql') ? array_filter([
+                (PHP_VERSION_ID >= 80500 ? \Pdo\Mysql::ATTR_SSL_CA : \PDO::MYSQL_ATTR_SSL_CA) => base_path('storage/cacert.pem'),
+            ]) : [],
         ],
 
         'mariadb' => [
